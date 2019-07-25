7b6918285df14be19f6c80167763ce77eda1c578
limc-b <limc-b@LIMC-B-P1>
2019-07-25 18:00:54 +0800

    模板类的前置声明

--- TemplateClass/TemplateClass.h
+++ TemplateClass/TemplateClass.h
@@ -1,5 +1,46 @@
 #pragma once
 
-class TriMesh;
+#include <iostream>
 
-void PrintTriMeshInfo(TriMesh* mesh);
+template <typename T>
+class LinkFrame;
+
+template <typename T>
+class State 
+{
+public:
+	State()
+	{ 
+		myFrame = new LinkFrame<T>();
+	}
+	~State()
+	{
+		if (myFrame != nullptr)
+		{
+			delete myFrame;
+			myFrame = nullptr;
+		}
+	}
+
+	void PrintState(const T& value)
+	{
+		myFrame->PrintContext(value);
+	}
+
+protected:
+	LinkFrame<T> *myFrame;
+
+};
+
+template <typename T>
+class LinkFrame
+{
+public:
+	LinkFrame() {}
+	~LinkFrame() {}
+
+	void PrintContext(const T& value)
+	{
+		std::cout << value << std::endl;
+	}
+};
--- TemplateClass/main.cpp
+++ TemplateClass/main.cpp
@@ -1,9 +1,10 @@
-
+#include <string>
 #include "TemplateClass.h"
 
 int main(int argc, char* argv[])
 {
-
+	State<std::string> state;
+	state.PrintState(std::string("Hello World"));
 
 
 
